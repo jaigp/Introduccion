cf14f863b87840bcf07585c76f4e74dadc120eee
Juan Ignacio Gonzalez Pan <jai.gonz.pan@gmail.com>
2024-03-20 08:58:04 -0300

    Creation of practico.php

---
+++ practico.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo <<<END Prueba sobre la funcionalidad "<<<END...END;",
+    supuestamente deja un renglon por medio. END;   ?>
+    <p>Y esta es la diferenca entre el "parrafo" de arriba y abajo</p>
+</body>
+</html>
