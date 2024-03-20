77e6a87597efd517410d37b872d687f8ff153136
Juan Ignacio Gonzalez Pan <jai.gonz.pan@gmail.com>
2024-03-20 09:03:38 -0300

    Bugg fixed

--- practico.php
+++ practico.php
@@ -6,8 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <?php echo <<<END Prueba sobre la funcionalidad "<<<END...END;",
-    supuestamente deja un renglon por medio. END;   ?>
+    <?php echo <<<END
+    Prueba sobre la funcionalidad,
+    supuestamente deja un renglon por medio. 
+    END;   ?>
     <p>Y esta es la diferenca entre el "parrafo" de arriba y abajo</p>
 </body>
 </html>
