5f1f3bbc34cc83eab9f0e4638b85c0e982865872
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-04-03 08:47:29 -0300

    Deleted a 'comment'

--- practico.php
+++ practico.php
@@ -8,14 +8,12 @@
 <body>
     <?php 
     // Prueba de comentario I
-    echo 'Prueba de comentario II';
-    /* Prueba de comentario III*/
+    /* Prueba de comentario II*/
     echo <<<END
     Prueba sobre la funcionalidad,
     supuestamente deja un renglon por medio. 
     HOLA que es esto
     END;   ?>
-    
     <p>Y esta es la diferenca entre el "parrafo" de arriba y abajo</p>
 </body>
 </html>
