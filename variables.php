617d115b56100a10683e12b5e4c6ce1cbda6bf66
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-04-03 09:03:49 -0300

    Adding html

--- variables.php
+++ variables.php
@@ -1,8 +1,18 @@
-<?php 
-    echo="Primer trabajo con variables."; 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo="Primer trabajo con variables."; 
 
-    $nombre='Manolo';
-    $edad=31; 
-    $altura=1.72;
-    $mayorEdad=true;
-?>
+        $nombre='Manolo';
+        $edad=31; 
+        $altura=1.72;
+        $mayorEdad=true;
+    ?>
+</body>
+</html>
