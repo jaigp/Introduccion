c1e467d93c4b0470edf112dc439daf3d7bca4584
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-04-10 09:14:16 -0300

    Printing some variables and using var_dump

---
+++ printActivity.php
@@ -0,0 +1,8 @@
+<?php 
+    $nombreCompleto = "Juan Ignacio Gonzalez Pan";
+    $numberClass = 3;
+    $height = 1.82;
+    var_dump($nombreCompleto, $numberClass, $height);    
+    // var_dump da informacion sobre el tipo, la cantidad de caracteres y el valor de la variable.
+    // Si es algun valor fuera de un string se va a mostrar sin la cantidad de caracteres, sino que directamente te muestra el valor.
+?>
