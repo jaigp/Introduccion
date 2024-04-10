00d5b9cf4cc14a5ad97757417b229b07c6bdb177
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-04-10 08:50:58 -0300

    Creating and redefining a constant to see what happend

---
+++ constantes.php
@@ -0,0 +1,9 @@
+<?php 
+    
+    define('PI', 3.1416);
+    define('PI', 3.14);
+    // PHP arroja un warning advirtiendote que ya esta asignada la constante y en que linea esta asignada
+    // Nos indica que el compilador detecta el cambio, sin embargo no lo cambia y que fue asignada anteriormente
+    // En situaciones donde o no conviene el cambiar la variable o en donde se desea un valor fijo
+    
+?>
