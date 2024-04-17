bc6d3ba455cb4168a5ffb5664e3d595fe0d78741
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-04-17 09:05:50 -0300

    Kind of exercise about concatenation

---
+++ concatenacion1.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h1>Dos empresas tech uruguayas</h1>
+        <?php 
+        echo 'PedidosYa ' . 'y ' . 'Scanntech';
+        /* 
+        Las comillas simples simplemente se utilizan para imprimir strings planas,
+        Mientras que las dobles te permiten ingresar caracteres escapados y variables.
+        */ 
+        ?>
+    </body>
+</html>
