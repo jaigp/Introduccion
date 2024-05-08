1a2284757cbd8e0c80a6bd06254bb81ee25ba92b
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-05-08 10:06:07 -0300

    Some minor changes

---
+++ arreglos/arreglos.php
@@ -0,0 +1,26 @@
+<?php 
+
+    //arrays indexados 
+    $pruebaArreglos = [
+        'Lunes', 
+        'Martes', 
+        'Miercoles', 
+        'Jueves', 
+        'Viernes', 
+        'Sabado', 
+        'Domingo'
+    ];
+    $planetas = []; // ==> $planetas = array(); 
+    $planetas[] = 'Marte';
+    $planetas[] = 'Tierra';
+    $planetas[] = 'Venus';
+
+    foreach ($planetas as $astro) {echo "$astro <br>";}
+    //echo count($planetas)
+    //var_dump($planetas);
+    //var_dump($pruebaArreglos);
+
+    //arryas asociativos
+    
+
+?>
