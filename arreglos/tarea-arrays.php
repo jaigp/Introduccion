57702199a6c28cb6f264c4d0463beebd8a8eb6f5
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-05-08 10:07:10 -0300

    Added some minor changes

--- arreglos/tarea-arrays.php
+++ arreglos/tarea-arrays.php
@@ -1,10 +1,43 @@
-<?php 
-    $amigos = ['Marcos', 'Dario', 'Juan'];
-    $ciudades = ['Montevideo', 'Colonia', 'Salto'];
-    shuffle($amigos);
-    shuffle($ciudades);
-    
-    echo $amigos[rand(0, 2)] . " se va de viaje <br>";
-    echo $amigos[rand(0, 2)] . " se va de viaje a la bonita ciudad de " . $ciudades[rand(0, 2)];
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            color: white;
+            padding: 0;
+            margin: 0;
+            width:100vw;
+            height: 100vh;
+            background: linear-gradient(190deg, #000, #00308F);
+            display: flex;
+            justify-content: center;
+            align-content: center;
+        }
+        section{
+            border: 2px white solid;
+            border-radius: 14px;
+            margin: 20%;
+            padding-inline: 50px;
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            justify-content:center;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+        $arrayAmigos = ['Marcos', 'Dario', 'Juan'];
+        $arrayCiudades = ['Montevideo', 'Colonia', 'Salto'];
+        shuffle($arrayAmigos);
+        shuffle($arrayCiudades);
+    ?>
+    <section>
+        <p id="amigo-viaje"> <?php echo $arrayAmigos[rand(0, 2)] . " se va de viaje..." . "<br>";?></p>
+        <p id="amigo-ciudad"><?php echo " A la bonita ciudad de " . $arrayCiudades[rand(0, 2)];?></p>
+    </section>
+</body>
+</html>
