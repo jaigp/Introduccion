73b8047a0b4e9442dea176f26a2c082a0413a4a5
Juan Gonzalez <jai.gonz.pan@gmail.com>
2024-05-29 09:31:51 -0300

    Practice excersice about strings

--- strings/strings.php
+++ strings/strings.php
@@ -1,10 +1,74 @@
-<?php
-/*$palabra = 'abcdef';
-var_dump($palabra);
-$palabra[2] = 'z';
-var_dump($palabra);
-*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            color: white;
+        }
+        body{
+            height: 100vh;
+            width: 100vw;
+            background-color: #25be5b;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        section{
+            padding: 20px;
+            border: 2px white solid;
+            border-radius: 15px;
+        }
+        section>form{
+            display: flex;
+            flex-direction: column;
+            justify-self: center;
+            align-items: center;
+            margin: 10px;
+        }
+        section>form>textarea{
+            margin: 10px;
+            padding: 20px;
+            border-radius: 15px;
+            color: black;
+        }
+        input[type="submit"]{
+            color: black;
+            border: 0;
+            padding: 5px;
+            border-radius: 15px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    /*$palabra = 'abcdef';
+    var_dump($palabra);
+    $palabra[2] = 'z';
+    var_dump($palabra);
+    */
+    if(isset($_POST['submit'])) {
+        $cuentoMostrar = $_POST['text-cuento'];
+        $contador = explode(" ", $cuentoMostrar);
+    }
+    ?>
+    <section>
+        <form action="" method="post" id="formulario">
+            <label for="text-cuento">Ingrese su texto</label>
+            <textarea name="text-cuento" cols="30" rows="5" maxlength="700" placeholder="Ingrese su texto..."></textarea>
+            <input type="submit" name="submit" value="Aceptar"></input>
+        </form>
+        <?php if (count($contador) > 100 || strlen($cuentoMostrar) > 700): ?>
+            <p><?php echo 'su cuento no cumple con los requisitos';?></p>
+        <?php endif;?>
+        <?php if(count($contador) <= 100) : ?>
+            <p><?php echo 'su cuento cumple con los requisitos'?></p>
+        <?php endif;?>
+    </section>
+</body>
+</html>
 
-$cuento = "Test, Springtime paints the world in vibrant hues, awakening nature from its winter slumber. Blossoming flowers infuse the air with delicate fragrances, while birds return, filling the sky with their cheerful melodies. The warming sun invites people outdoors, where parks and gardens come alive with families and friends enjoying picnics and strolls. Trees don fresh, green foliage, creating a lush canopy overhead. Spring is a season of renewal and growth, a reminder of nature's resilience and beauty. It's a time to embrace new beginnings, savoring the simple pleasures of longer days and the promise of summer just around the corner.";
-$arrayCuento = explode(" ", $cuento);
-print_r(count($arrayCuento));
